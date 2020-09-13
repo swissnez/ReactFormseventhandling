b6470fad793686b077f1c075cf5b9b34625b35eb
swissnez <neal@abovebeyondtech.se>
2020-09-13 17:41:32 +0200

    objEventTarget Grab the name and the values from the input HTML markup on the event driven method, save to an object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,16 @@ import _ from "lodash";
 
 function App() {
 
-const [fName,setFname] = useState("");
-const [lName,setLname] = useState("");
+/* const [fName,setFname] = useState("");
+const [lName,setLname] = useState(""); */
 const [submitted,setSubmit] = useState(false);
+
+// Using an {} obj fullName.fName e.g 
+const [fullName,setFullName] = useState({
+  fName: "",
+  lName: ""
+});
+
 
   function handleClick(event){
     setSubmit(true);
@@ -15,15 +22,18 @@ const [submitted,setSubmit] = useState(false);
 
 
   function handleChange(event) {
-    (event.target.name === "fName") ? setFname(event.target.value) : setLname(event.target.value);
+    const {name,value} = event.target; // extract contant into an object
+    // (event.target.name === "fName") ? setFname(event.target.value) : setLname(event.target.value);
+    (name === "fName") ? fullName.fName = value : fullName.lName = value;
   }
 
   return (
     <div className="container">
-      <h1>Hello {submitted? _.capitalize(fName) + " " + _.capitalize(lName):"" }</h1>
+      {/* <h1>Hello {submitted? _.capitalize(fName) + " " + _.capitalize(lName):"" }</h1> */}
+      <h1>Welcome {submitted? _.capitalize(fullName.fName) + " " + _.capitalize(fullName.lName):"" }</h1>
       <form onChange={handleChange}>
-        <input name="fName" placeholder="First Name" value={fName} />
-        <input name="lName" placeholder="Last Name" value={lName} />
+        <input name="fName" placeholder="Your first name"  />
+        <input name="lName" placeholder="Your last name"  />
         <button onClick={handleClick}>Submit</button>
       </form>
     </div>
