85074d38f28d6ab65aaeb8e4add2c6ecaec925b4
swissnez <neal@abovebeyondtech.se>
2020-09-13 15:17:31 +0200

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,8 +22,8 @@ const [submitted,setSubmit] = useState(false);
     <div className="container">
       <h1>Hello {submitted? _.capitalize(fName) + " " + _.capitalize(lName):"" }</h1>
       <form onChange={handleChange}>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input name="fName" placeholder="First Name" value={fName} />
+        <input name="lName" placeholder="Last Name" value={lName} />
         <button onClick={handleClick}>Submit</button>
       </form>
     </div>
