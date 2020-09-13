41a579ba59408590aaa7f2b41417feae4b1a5e57
swissnez <neal@abovebeyondtech.se>
2020-09-13 15:13:27 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import _ from "lodash";
+
+
+function App() {
+
+const [fName,setFname] = useState("");
+const [lName,setLname] = useState("");
+const [submitted,setSubmit] = useState(false);
+
+  function handleClick(event){
+    setSubmit(true);
+    event.preventDefault() 
+  }
+
+
+  function handleChange(event) {
+    (event.target.name === "fName") ? setFname(event.target.value) : setLname(event.target.value);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {submitted? _.capitalize(fName) + " " + _.capitalize(lName):"" }</h1>
+      <form onChange={handleChange}>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <button onClick={handleClick}>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+
+export default App;
+
